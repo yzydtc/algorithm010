8d5fbf027655d439e9c4b79774f1ee6306a9331d
杨智源 <531156799@qq.com>
2020-06-28 23:42:07 +0800

    “作业提交

---
+++ Week03/236.二叉树的最近公共祖先.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=java
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    private TreeNode ans;
+
+    public Solution() {
+        this.ans = null;
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        // 1.terminator
+        if (root == null)
+            return false;
+        // 2.local logic
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
+        // 3.drill down
+        return lson || rson || (root.val == p.val || root.val == q.val);
+        // 4.reserve status
+    }
+
+}
+// @lc code=end
